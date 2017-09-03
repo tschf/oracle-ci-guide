0965380c5d29a46b4b42d3d12404bb6f2e71606e
Trent Schafer <trent.schafer@gmail.com>
2017-09-03 17:50:45 -0400

    Move XE download to download.sh

--- .travis/oracle/download.sh
+++ .travis/oracle/download.sh
@@ -1,16 +1,3 @@
-#!/bin/sh -e
-
-[ -n "$ORACLE_COOKIE" ] || { echo "Missing ORACLE_COOKIE environment variable!"; exit 1; }
-[ -n "$ORACLE_FILE" ] || { echo "Missing ORACLE_FILE environment variable!"; exit 1; }
-
-cd "$(dirname "$(readlink -f "$0")")"
-
-npm install bluebird node-phantom-simple
-
-export COOKIES='cookies.txt'
-export USER_AGENT='Mozilla/5.0'
-
-echo > "$COOKIES"
-chmod 600 "$COOKIES"
-
-exec node download.js
+#!/bin/bash
+odl -component db -version 11gXE -os linux -arch x64 -lang na -accept-license
+file $XE_ZIP
