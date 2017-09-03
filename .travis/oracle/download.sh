98328a3a5e3f46fb19247e40da5cf0de7332ce74
Trent Schafer <trent.schafer@gmail.com>
2017-09-03 17:54:28 -0400

    Add cache check

--- .travis/oracle/download.sh
+++ .travis/oracle/download.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# Check if the file exists already
+if [[ -f $CACHE_DIR/$XE_ZIP ]]
+then
+    echo "File cached. Don't need to re-fetch"
+    exit 0
+fi
+
 odl -component db -version 11gXE -os linux -arch x64 -lang na -accept-license
 file $XE_ZIP
