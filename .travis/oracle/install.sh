66b7f135e27db25c5d5cb3eafd4ca1fb84ae2634
Trent Schafer <trent.schafer@gmail.com>
2017-09-03 18:57:50 -0400

    Remove unzip commands of XE

--- .travis/oracle/install.sh
+++ .travis/oracle/install.sh
@@ -6,12 +6,3 @@ df -B1 /dev/shm | awk 'END { if ($1 != "shmfs" && $1 != "tmpfs" || $2 < 21474836
     ( echo '#!/bin/sh' | sudo tee /sbin/chkconfig > /dev/null && sudo chmod u+x /sbin/chkconfig )
 
 test -d /var/lock/subsys || sudo mkdir /var/lock/subsys
-
-cd ${HOME}
-mkdir xe_files
-cd xe_files
-file ${CACHE_DIR}/${XE_ZIP}
-unzip -j ${CACHE_DIR}/${XE_ZIP} "Disk1/${ORACLE_RPM}"
-ls
-
-sudo rpm --install --nodeps --nopre "${ORACLE_RPM}"
